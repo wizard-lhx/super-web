2f31762521126b4d72aa510183314628d76bdb41
wizard-lhx <wizardhxliang@gmail.com>
2024-10-14 14:12:21 +0800

    增加前端页面

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,6 +1,39 @@
 <template>
   <div class="home">
-    主页
+    <el-container>
+<!--      侧边栏隐藏滚动条-->
+      <el-aside style="overflow: hidden">
+        <div index="firstPage" style="height: 45px;display: flex;justify-content: center;align-items: center">
+          <img src="@/assets/sdu_logo.jpg" alt="" style="width: 30px;height: 30px">
+          <span>系统首页</span>
+        </div>
+        <el-menu>
+          <el-submenu index="/">
+            <template slot="title">
+              <i class="el-icon-position"></i>
+              <span>读者专区</span>
+            </template>
+          </el-submenu>
+          <el-submenu index="element">
+            <template slot="title">
+              <i class="el-icon-user"></i>
+              <span>管理员专区</span>
+            </template>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+
+      <el-container>
+<!--        头部区域-->
+        <el-header style="height: 45px;display: flex;align-items:center;justify-content: flex-end">
+          <i class="el-icon-user" @click="login"></i>
+        </el-header>
+<!--        主体-->
+        <el-main>
+
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
@@ -8,5 +41,14 @@
 
 export default {
   name: 'HomeView',
+  methods:{
+    login(){
+      console.log('1')
+    }
+  }
 }
 </script>
+
+<style>
+
+</style>
---
+++ vue/src/views/test_element.vue
@@ -0,0 +1,83 @@
+<script>
+export default {
+  name: "test_element"
+}
+</script>
+
+<template>
+  <div>
+    <el-row>
+      <el-col :span="12">
+        <div style="background-color: aqua;width: 100%;height: 100px"></div>
+      </el-col>
+    </el-row>
+    <el-row :gutter='20'>
+      <el-col :span="12">
+        <div style="margin-top:20px;background-color: aqua;width: 100%;height: 100px"></div>
+      </el-col>
+      <el-col :span="12">
+        <div style="margin-top:20px;background-color: aqua;width: 100%;height: 100px"></div>
+      </el-col>
+    </el-row>
+
+    <el-container>
+      <el-header style="display:flex;color: brown;align-items:center;justify-content: center;background-color: gray">Header</el-header>
+      <el-main style="color: black;align-items: center;background-color: darkgreen">Main</el-main>
+    </el-container>
+
+<!--    <i class="el-icon-alarm-clock"></i>-->
+
+    <el-row>
+      <el-col>
+        <el-button type="danger">danger button</el-button>
+      </el-col>
+    </el-row>
+
+    <el-row>
+      <el-col>
+        <el-radio v-model="radio" @change="radioChange" label="男">男</el-radio>
+        <el-radio v-model="radio" @change="radioChange" label="女">女</el-radio>
+      </el-col>
+    </el-row>
+
+    <el-input style="width:300px" v-model="input" placeholder="please input your name"></el-input>
+
+    <el-select v-model="select">
+      <el-option v-for="option in fruits" :key="option.value" :value="option.value" :label="option.name"></el-option>
+    </el-select>
+
+    <el-date-picker v-model="date1" type="daterange" value-format="yyyy-MM-dd" @change="dateChange" ></el-date-picker>
+
+    <el-table :data="fruits">
+      <el-table-column label="Name" prop="name"></el-table-column>
+      <el-table-column label="Num" prop="value"></el-table-column>
+    </el-table>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data(){
+    return {
+      radio: '1',
+      input: '',
+      fruits:[
+        {'name':'apple', 'value':'1'},
+        {'name':'pineapple', 'value':'2'},
+        {'name':'banana', 'value':'3'},
+      ],
+      select:'',
+      date1:''
+    }
+  },
+  methods:{
+    radioChange(){
+      console.log(this.radio)
+    },
+    dateChange(){
+      console.log(this.date1)
+    }
+  }
+}
+</script>
