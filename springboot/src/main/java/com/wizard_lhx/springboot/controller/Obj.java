9472ae363aae494a5900bdbd144125f0640eddea
wizard-lhx <wizardhxliang@gmail.com>
2024-10-15 20:08:30 +0800

    增加http

---
+++ springboot/src/main/java/com/wizard_lhx/springboot/controller/Obj.java
@@ -0,0 +1,18 @@
+package com.wizard_lhx.springboot.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 功能：
+ * 作者：wizard-lhx
+ * 日期：2024/10/15 10:52
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Obj {
+    private String name;
+    private int age;
+}
--- springboot/src/main/java/com/wizard_lhx/springboot/controller/WebController.java
+++ springboot/src/main/java/com/wizard_lhx/springboot/controller/WebController.java
@@ -1,8 +1,7 @@
 package com.wizard_lhx.springboot.controller;
 
 import com.wizard_lhx.springboot.common.Result;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 功能：提供返回数据接口
@@ -11,8 +10,22 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class WebController {
-    @RequestMapping
-    public Result hello(String name) {
-        return Result.success(name);
+    @GetMapping("/get")
+    public Result get(@RequestBody Obj obj) {
+        return Result.success(obj);
+    }
+
+    @PostMapping("/post")
+    public Result post(@RequestBody Obj obj) {
+        return Result.success(obj);
+    }
+
+    @PutMapping("/put")
+    public Result put(@RequestBody Obj obj) {
+        return Result.success(obj);
+    }
+    @DeleteMapping("/delete")
+    public Result delete(@RequestBody Obj obj) {
+        return Result.success(obj);
     }
 }
